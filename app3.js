156bf67679a7b210d92947d37353ce02f93123be
Marta <mrtdeneke@gmail.com>
2021-11-18 11:28:44 -0800

    had to fix issues on app 3

--- app3.js
+++ app3.js
@@ -1,6 +1,5 @@
-let additionalPicture = ('Do you want to see another Kemis?');
+let additionalPicture = prompt('Do you want to see another Kemis?');
 let picURl = " https://d1v9pyzt136u2g.cloudfront.net/blog/wp-content/uploads/2021/07/23095000/habesha.jpg "
 if (additionalPicture.toLowerCase() == "yes") {
-
-    document.write('<img src= "' + picURl + '">')
+    document.write('<img src="' + picURl + '">')
 }
