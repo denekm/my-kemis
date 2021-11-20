7e0046cbdb58fdccd91dcbd16cc1b1e1851393c3
Marta <mrtdeneke@gmail.com>
2021-11-19 16:33:02 -0800

    needed change on app1

--- app1.js
+++ app1.js
@@ -23,8 +23,7 @@ if(userAnswer.toUpperCase() == "YES"){
 }
 
 
-
-function anotherPictureQuestion(){
+    let anotherPictureQuestion = function(){
         let additionalPicture = prompt('Do you want to see another Kemis?');
         let picURl = " https://d1v9pyzt136u2g.cloudfront.net/blog/wp-content/uploads/2021/07/23095000/habesha.jpg "
        
